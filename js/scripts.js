a5a0f63584b4f77f881da75f5684a443f3544805
MontserratJB <montsejimenezb@gmail.com>
2021-06-14 18:15:20 -0600

    Actualizar

--- js/scripts.js
+++ js/scripts.js
@@ -28,12 +28,17 @@ var capas_base = {
 };	    
 
 
-// Ícono personalizado para carnivoros
-const iconoCarnivoro = L.divIcon({
-  html: '<i class="fas fa-paw fa-1x"></i>',
+// Ícono personalizado para Cuerpos de agua
+const iconoCagua = L.divIcon({
+  html: '<i class="fas fa-tint fa-2x"></i>',
   className: 'estiloIconos'
 });
 
+// Ícono personalizado para Centros educativos
+const iconoColegios = L.divIcon({
+  html: '<i class="fas fa-school fa-2x"></i>',
+  className: 'estiloIconos'
+});
 
 // Control de capas
 control_capas = L.control.layers(capas_base).addTo(mapa);	
@@ -43,40 +48,58 @@ control_capas = L.control.layers(capas_base).addTo(mapa);
 L.control.scale().addTo(mapa);
    
 
-// Capa vectorial de registros agrupados de carnívoros
-$.getJSON("https://tpb729-desarrollosigweb-2021.github.io/datos/gbif/carnivora-cr-wgs84.geojson", function(geodata) {
+// Capa vectorial de registros de Cuerpos de agua
+$.getJSON("https://raw.githubusercontent.com/MontserratJB/tarea_04/master/ign/otros_cuerpos_de_agua.geojson", function(geodata) {
   // Capa de registros individuales
-  var capa_carnivora = L.geoJson(geodata, {
+  var cuerpos_agua = L.geoJson(geodata, {
     style: function(feature) {
 	  return {'color': "#013220", 'weight': 3}
     },
     onEachFeature: function(feature, layer) {
-      var popupText = "<strong>Especie</strong>: " + feature.properties.species + "<br>" + 
-                      "<strong>Localidad</strong>: " + feature.properties.locality + "<br>" + 
-                      "<strong>Fecha</strong>: " + feature.properties.eventDate + "<br>" + 
-                      "<strong>Institución</strong>: " + feature.properties.institutionCode + "<br>" + 
-                      "<br>" +
-                      "<a href='" + feature.properties.occurrenceID + "'>Más información</a>";
+      var popupText = "<strong>Codigo</strong>: " + feature.properties.codigo + "<br>" + 
+                      "<strong>Origen</strong>: " + feature.properties.origen + "<br>" + 
+                      "<br>";
       layer.bindPopup(popupText);
     },
     pointToLayer: function(getJsonPoint, latlng) {
-        return L.marker(latlng, {icon: iconoCarnivoro});
+        return L.marker(latlng, {icon: iconoCagua});
+    }
+  });
+
+  // Capa de puntos agrupados
+  var capa_cagua_agrupados = L.markerClusterGroup({spiderfyOnMaxZoom: true});
+  capa_cagua_agrupados.addLayer(cuerpos_agua);
+
+  // Se añaden las capas al mapa y al control de capas
+  capa_cagua_agrupados.addTo(mapa);
+  control_capas.addOverlay(capa_cagua_agrupados, 'Cuerpos de agua');
+
+});
+
+// Capa vectorial de centros educativos de CR
+$.getJSON("https://raw.githubusercontent.com/MontserratJB/tarea_04/master/ign/colegios_mep.geojson", function(geodata) {
+  // Capa de registros individuales
+  var capa_colegios = L.geoJson(geodata, {
+    style: function(feature) {
+	  return {'color': "#013220", 'weight': 3}
+    },
+    pointToLayer: function(getJsonPoint, latlng) {
+        return L.marker(latlng, {icon: iconoColegios});
     }
   });
 
   // Capa de calor (heatmap)
   coordenadas = geodata.features.map(feat => feat.geometry.coordinates.reverse());
-  var capa_carnivora_calor = L.heatLayer(coordenadas, {radius: 30, blur: 1});
-
-  // Capa de puntos agrupados
-  var capa_carnivora_agrupados = L.markerClusterGroup({spiderfyOnMaxZoom: true});
-  capa_carnivora_agrupados.addLayer(capa_carnivora);
+  var capa_colegios_calor = L.heatLayer(coordenadas, {radius: 40, blur: 20, max: 0.03});
 
   // Se añaden las capas al mapa y al control de capas
-  capa_carnivora_calor.addTo(mapa);
-  control_capas.addOverlay(capa_carnivora_calor, 'Mapa de calor');
-  // capa_carnivora_agrupados.addTo(mapa);
-  control_capas.addOverlay(capa_carnivora_agrupados, 'Registros agrupados');
-  // capa_carnivora.addTo(mapa);
-  control_capas.addOverlay(capa_carnivora, 'Registros individuales de carnívoros');
+  capa_colegios_calor.addTo(mapa);
+  control_capas.addOverlay(capa_colegios_calor, 'Mapa de calor CE');
+  
 });
+  
+  
+  
+  
+  
+  
